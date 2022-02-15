a0c73b9899870bbf8c9de966d0f6737fd17da49a
Gleano Malke <gleano.100@hotmail.com>
2022-02-15 16:24:40 +0100

    Added validations for JobDTO
    Added validations for JobDTO.

--- src/model/JobDTO.js
+++ src/model/JobDTO.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Validators = require('../util/Validators');
 /**
  * Representing the job and the respective competences related to it.
  */
@@ -8,10 +9,13 @@ class JobDTO {
    * Create an instance of the job with the related competences.
    * @param {Integer} jobID The id related to the job.
    * @param {String} description The name of the job.
-   * @param {{"id", "type"}} competences An array holding all the competences with 
+   * @param {[{id, type}]} competences An array holding all the competences with 
    *                                     objects that have the id and type of the competence.
    */
   constructor(jobID, description, competences) {
+    Validators.isPositiveWholeNumber(jobID, 'Job ID');
+    Validators.isDescriptionString(description, 'Job description');
+    competences.forEach(competence => Validators.isCompetence(competence, 'Competence'));
     this.jobID = jobID;
     this.description = description;
     this.competences = competences;
